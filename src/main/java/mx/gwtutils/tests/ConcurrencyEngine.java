eaf471ae58ce415f121a48508ce852a2ce491101
mxro <mxro@nowhere.com>
2011-09-02 23:11:56 +0000

    improving integration test cases

--- src/main/java/mx/gwtutils/tests/ConcurrencyEngine.java
+++ src/main/java/mx/gwtutils/tests/ConcurrencyEngine.java
@@ -2,19 +2,45 @@ package mx.gwtutils.tests;
 
 import mx.gwtutils.ThreadSpace.Step;
 
+/**
+ * A simple abstraction for a number of concurrency operations. These are
+ * supported by GWT applications as well as by JSE applications.
+ * 
+ * @author mroh004
+ * 
+ */
+public abstract class ConcurrencyEngine {
 
-public abstract class ConcurrencyEngine implements DelayTest {
 	public abstract AbstractTimer newTimer(Runnable timer);
 
-	
+	/**
+	 * Enters the thread into a loop, which is executed for the specified
+	 * duration. If the time specified in the duration is passed, an exception
+	 * will be thrown. To prevent this {@link #finishTest()} should be called
+	 * within the specified time frame.<br />
+	 * <br /> {@link #delayTestFinish(int)} should sensibly be called as the last
+	 * instruction in a test case.
+	 * 
+	 * @param duration
+	 *            Duration in ms for which the test case waits
+	 */
+	public abstract void delayTestFinish(int duration);
+
+	/**
+	 * Finishes a delayed test successfully.
+	 * 
+	 * @see #delayTestFinish(int)
+	 */
+	public abstract void finishTest();
+
 	public void sleep(final int millisec, final Step after) {
 		final AbstractTimer t = newTimer(new Runnable() {
-	
+
 			@Override
 			public void run() {
 				after.process();
 			}
-	
+
 		});
 		t.schedule(millisec);
 	}
--- src/main/java/mx/gwtutils/tests/DelayTest.java
+++ src/main/java/mx/gwtutils/tests/DelayTest.java
@@ -1,6 +1,5 @@
 package mx.gwtutils.tests;
 
 public interface DelayTest {
-	public void delayTestFinish(int duration) ;
-	public void finishTest();
+	
 }
