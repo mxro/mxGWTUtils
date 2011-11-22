e2f57e082e79aae9fb23e5c1a6d5a9c780ef3d61
mxro <mxro@nowhere.com>
2011-11-22 04:25:35 +0000

    refactoring for release
    cleanup apis and improve JavaDoc

---
+++ src/main/java/mx/gwtutils/ConditionalThread.java
@@ -0,0 +1,67 @@
+package mx.gwtutils;
+
+import java.util.LinkedList;
+import java.util.List;
+
+
+public class ConditionalThread {
+
+	protected final List<ConditionalStep> steps;
+	protected volatile boolean processing;
+	protected static final int MAX_RETRIES = 500;
+	protected volatile int retries;
+	
+	public static interface ConditionalStep {
+		/**
+		 * Should return true if operation was successful, otherwise false.
+		 * 
+		 * @return
+		 */
+		public boolean process();
+	}
+
+	public ConditionalThread() {
+		steps = new LinkedList<ConditionalStep>();
+		retries = MAX_RETRIES;
+	}
+
+	private void processStepsGuarded() {
+		if (steps.size() == 0) {
+			return;
+		}
+		final ConditionalStep step = steps.get(0);
+		//System.out.println("steps remaining: "+steps.size());
+		if (step.process()) {
+			steps.remove(0);
+			retries = MAX_RETRIES;
+					
+			processStepsGuarded();
+		} else {
+			retries--;
+			assert retries > 0 : "Thread aborted since maximum retires ("+MAX_RETRIES+") reached.";			
+		}
+
+	}
+
+	public synchronized void processSteps() {
+		if (processing) {
+			return;
+		}
+
+		try {
+			processing = true;
+
+			if (steps.size() > 0) {
+				processStepsGuarded();
+			}
+
+		} finally {
+			processing = false;
+		}
+	}
+
+	public synchronized void add(final ConditionalStep s) {
+		this.steps.add(s);
+	}
+
+}
