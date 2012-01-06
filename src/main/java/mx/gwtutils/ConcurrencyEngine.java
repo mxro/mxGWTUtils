d155cd06ef2e5b8901cc337c598ab25a299df20c
mxro <mxro@nowhere.com>
2012-01-06 01:01:39 +0000

    non live replication test case passes!
    now in process implementing live replication test case and finishing up implementation of required features.

--- src/main/java/mx/gwtutils/ConcurrencyEngine.java
+++ src/main/java/mx/gwtutils/ConcurrencyEngine.java
@@ -16,35 +16,57 @@ import mx.gwtutils.tests.AbstractTimer;
  * 
  */
 public abstract class ConcurrencyEngine {
-	
+
+	boolean printLog = false;
+
+	/**
+	 * All future log entries will be written to the console.
+	 */
+	public void printLog() {
+		printLog = true;
+	}
+
+	public void will(final String doWhat) {
+		if (printLog) {
+			System.out.println("Will '" + doWhat + "'");
+		}
+	}
+
+	public void has(final String doneWhat) {
+		if (printLog) {
+			System.out.println("Has '" + doneWhat + "'");
+		}
+	}
+
 	public abstract void yield();
-	
+
 	public abstract AbstractTimer newTimer(Runnable timer);
 
 	public interface Verifyer {
 		public void join();
 	}
-	
+
 	public Verifyer runAsync(final Step step) {
 		sleep(1, step);
 		return new Verifyer() {
 
 			@Override
 			public void join() {
-				
+
 			}
-			
+
 		};
 	}
-	
+
 	/**
 	 * 
-	 * @param millisec For GWT, this needs to be a positive value greater than 0.
+	 * @param millisec
+	 *            For GWT, this needs to be a positive value greater than 0.
 	 * @param after
 	 */
 	public void sleep(final int millisec, final Step after) {
 		assert millisec > 0 : "Millisec should be greater than 0.";
-		
+
 		final AbstractTimer t = newTimer(new Runnable() {
 
 			@Override
@@ -55,13 +77,14 @@ public abstract class ConcurrencyEngine {
 		});
 		t.schedule(millisec);
 	}
-	
+
 	/**
 	 * Enters the thread into a loop, which is executed for the specified
 	 * duration. If the time specified in the duration is passed, an exception
 	 * will be thrown. To prevent this {@link #finishTest()} should be called
 	 * within the specified time frame.<br />
-	 * <br /> {@link #delayTestFinish(int)} should sensibly be called as the last
+	 * <br />
+	 * {@link #delayTestFinish(int)} should sensibly be called as the last
 	 * instruction in a test case.
 	 * 
 	 * @param duration
@@ -77,9 +100,11 @@ public abstract class ConcurrencyEngine {
 	public abstract void finishTest();
 
 	/**
-	 * If there is a currently delayed test case, this method will fail this test.
+	 * If there is a currently delayed test case, this method will fail this
+	 * test.
+	 * 
 	 * @param t
 	 */
 	public abstract void failTest(Throwable t);
-	
+
 }
