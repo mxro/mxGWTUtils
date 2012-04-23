eaaa23b5b06a2b6c6647555c5d4fc7f0423520cf
mxro <mxro@nowhere.com>
2012-04-23 06:45:53 +0000

    fixed error in unit test caused by changing the way clients are shutdown when there was an active transaction while shutdown, an exception would occur now an error message is issued to the user

--- src/main/java/mx/gwtutils/Nullable.java
+++ src/main/java/mx/gwtutils/Nullable.java
@@ -1,19 +1,11 @@
-/*******************************************************************************
- * Copyright 2011 Max Erik Rohde http://www.mxro.de
- * 
- * All rights reserved.
- ******************************************************************************/
 package mx.gwtutils;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
-
-
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Nullable {
 
 }
-
