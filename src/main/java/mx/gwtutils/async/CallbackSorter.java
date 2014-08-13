64d65c137c0a597d477c9a5ecd4bf86fcce57b9f
mxro <mxro@nowhere.com>
2014-08-13 10:36:27 +1000

    a

--- src/main/java/mx/gwtutils/async/CallbackSorter.java
+++ src/main/java/mx/gwtutils/async/CallbackSorter.java
@@ -1,8 +1,4 @@
-/*******************************************************************************
- * Copyright 2011 Max Erik Rohde http://www.mxro.de
- * 
- * All rights reserved.
- ******************************************************************************/
+
 package mx.gwtutils.async;
 
 import java.util.ArrayList;
