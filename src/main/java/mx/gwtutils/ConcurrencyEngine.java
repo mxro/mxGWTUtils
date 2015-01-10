72e69ec011795c729708aa855e3856bfe2705428
mxro <mxro@nowhere.com>
2015-01-10 16:24:22 +1100

    Resolving compatability issues with JavaScript Autocommit Sat Jan 10 16:24:22 EST 2015

--- src/main/java/mx/gwtutils/ConcurrencyEngine.java
+++ src/main/java/mx/gwtutils/ConcurrencyEngine.java
@@ -5,10 +5,11 @@
  ******************************************************************************/
 package mx.gwtutils;
 
-import de.mxro.concurrency.schedule.Step;
 import mx.gwtutils.tests.AbstractTimer;
+import de.mxro.concurrency.schedule.Step;
 
 /**
+ * <p>
  * A simple abstraction for a number of concurrency operations. These are
  * supported by GWT applications as well as by JSE applications.
  * 
@@ -17,94 +18,94 @@ import mx.gwtutils.tests.AbstractTimer;
  */
 public abstract class ConcurrencyEngine {
 
-	boolean printLog = false;
+    boolean printLog = false;
 
-	/**
-	 * All future log entries will be written to the console.
-	 */
-	public void printLog() {
-		printLog = true;
-	}
+    /**
+     * All future log entries will be written to the console.
+     */
+    public void printLog() {
+        printLog = true;
+    }
 
-	public void will(final String doWhat) {
-		if (printLog) {
-			System.out.println("Will '" + doWhat + "'");
-		}
-	}
+    public void will(final String doWhat) {
+        if (printLog) {
+            System.out.println("Will '" + doWhat + "'");
+        }
+    }
 
-	public void has(final String doneWhat) {
-		if (printLog) {
-			System.out.println("Has '" + doneWhat + "'");
-		}
-	}
+    public void has(final String doneWhat) {
+        if (printLog) {
+            System.out.println("Has '" + doneWhat + "'");
+        }
+    }
 
-	public abstract void yield();
+    public abstract void yield();
 
-	public abstract AbstractTimer newTimer(Runnable timer);
+    public abstract AbstractTimer newTimer(Runnable timer);
 
-	public interface Verifyer {
-		public void join();
-	}
+    public interface Verifyer {
+        public void join();
+    }
 
-	public Verifyer runAsync(final Step step) {
-		sleep(1, step);
-		return new Verifyer() {
+    public Verifyer runAsync(final Step step) {
+        sleep(1, step);
+        return new Verifyer() {
 
-			@Override
-			public void join() {
+            @Override
+            public void join() {
 
-			}
+            }
 
-		};
-	}
+        };
+    }
 
-	/**
-	 * 
-	 * @param millisec
-	 *            For GWT, this needs to be a positive value greater than 0.
-	 * @param after
-	 */
-	public void sleep(final int millisec, final Step after) {
-		assert millisec > 0 : "Millisec should be greater than 0.";
+    /**
+     * 
+     * @param millisec
+     *            For GWT, this needs to be a positive value greater than 0.
+     * @param after
+     */
+    public void sleep(final int millisec, final Step after) {
+        assert millisec > 0 : "Millisec should be greater than 0.";
 
-		final AbstractTimer t = newTimer(new Runnable() {
+        final AbstractTimer t = newTimer(new Runnable() {
 
-			@Override
-			public void run() {
-				after.process();
-			}
+            @Override
+            public void run() {
+                after.process();
+            }
 
-		});
-		t.schedule(millisec);
-	}
+        });
+        t.schedule(millisec);
+    }
 
-	/**
-	 * Enters the thread into a loop, which is executed for the specified
-	 * duration. If the time specified in the duration is passed, an exception
-	 * will be thrown. To prevent this {@link #finishTest()} should be called
-	 * within the specified time frame.<br />
-	 * <br />
-	 * {@link #delayTestFinish(int)} should sensibly be called as the last
-	 * instruction in a test case.
-	 * 
-	 * @param duration
-	 *            Duration in ms for which the test case waits
-	 */
-	public abstract void delayTestFinish(int duration);
+    /**
+     * Enters the thread into a loop, which is executed for the specified
+     * duration. If the time specified in the duration is passed, an exception
+     * will be thrown. To prevent this {@link #finishTest()} should be called
+     * within the specified time frame.<br />
+     * <br />
+     * {@link #delayTestFinish(int)} should sensibly be called as the last
+     * instruction in a test case.
+     * 
+     * @param duration
+     *            Duration in ms for which the test case waits
+     */
+    public abstract void delayTestFinish(int duration);
 
-	/**
-	 * Finishes a delayed test successfully.
-	 * 
-	 * @see #delayTestFinish(int)
-	 */
-	public abstract void finishTest();
+    /**
+     * Finishes a delayed test successfully.
+     * 
+     * @see #delayTestFinish(int)
+     */
+    public abstract void finishTest();
 
-	/**
-	 * If there is a currently delayed test case, this method will fail this
-	 * test.
-	 * 
-	 * @param t
-	 */
-	public abstract void failTest(Throwable t);
+    /**
+     * If there is a currently delayed test case, this method will fail this
+     * test.
+     * 
+     * @param t
+     */
+    public abstract void failTest(Throwable t);
 
 }
