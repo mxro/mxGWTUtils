d7b6ddf0952df4d896e688cc84c52054b172030a
mxro <mxro@nowhere.com>
2015-07-30 07:52:10 +1000

    Implementing new login Autocommit Thu Jul 30 07:52:10 EST 2015

--- src/main/java/mx/gwtutils/MxroGWTUtils.java
+++ src/main/java/mx/gwtutils/MxroGWTUtils.java
@@ -12,7 +12,6 @@ import java.util.List;
 import java.util.Vector;
 
 import one.utils.OneUtilsCollections;
-import one.utils.OneUtilsStrings;
 
 public class MxroGWTUtils {
 
@@ -51,46 +50,6 @@ public class MxroGWTUtils {
         }
 
         return res;
-    }
-
-    /**
-     * @see #getSimpleName(String)
-     * 
-     */
-    public static String getSimpleName(final String forName, final int maxCharacters) {
-        final String name = getSimpleName(forName);
-        if (name.length() > maxCharacters) {
-            return name.substring(0, maxCharacters);
-        }
-        return name;
-    }
-
-    /**
-     * Simplifies any given string and makes it conformant as file name for an
-     * URI. Illegal characters are replaced by an '_'.<br/>
-     * <br />
-     * For legal characters, see {@link #allowedCharacters}:<br/>
-     * {@value #allowedCharacters}
-     * 
-     * @param forName
-     * @return
-     */
-    public static String getSimpleName(final String forName) {
-        final String n = forName;
-        if (n.length() > 0) {
-            String simple = "";
-            for (int i = 0; i < n.length(); i++) {
-                final boolean found = OneUtilsStrings.isSimpleCharacter(n.charAt(i));
-                if (found) {
-                    simple = simple + n.charAt(i);
-                } else {
-                    simple = simple + '_';
-                }
-            }
-            return simple;
-        } else {
-            return n;
-        }
     }
 
     public static int length(final String s, final String separator) {
